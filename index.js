39ac0cd15c3d21e1740a4922c8c4e507cfe3df6f
Trust7 <simeontrust7@gmail.com>
2022-10-10 22:08:59 +0100

    made fixes to linters errors

--- index.js
+++ index.js
@@ -4,32 +4,31 @@ const portfolioLink = document.getElementById('first-link');
 const aboutLink = document.getElementById('second-link');
 const contactLink = document.getElementById('third-link');
 
-
 let isShown = false;
 
-menuBar.addEventListener("click", function(){ 
-   isShown = !isShown
-   if(isShown === true){
-     dropDown.style.transform = "translateX(0%)" 
-     menuBar.childNodes[1].style.color = 'white'
-     menuBar.childNodes[1].className = "fa-regular fa-x"
-     menuBar.childNodes[1].style.fontSize = '20px'
-   }
-   else{
-     dropDown.style.transform = "translateX(100%)"
-     menuBar.childNodes[1].style.color = '#6070ff'
-     menuBar.childNodes[1].className = "fa-solid fa-bars"
-     menuBar.childNodes[1].style.fontSize = '30px'
-   }
-  
-});
+const toggleDropDown = () => {
+  isShown = !isShown;
+  if (isShown === true) {
+    dropDown.style.transform = 'translateX(0%)';
+    menuBar.childNodes[1].style.color = 'white';
+    menuBar.childNodes[1].className = 'fa-regular fa-x';
+    menuBar.childNodes[1].style.fontSize = '20px';
+  } else {
+    dropDown.style.transform = 'translateX(100%)';
+    menuBar.childNodes[1].style.color = '#6070ff';
+    menuBar.childNodes[1].className = 'fa-solid fa-bars';
+    menuBar.childNodes[1].style.fontSize = '30px';
+  }
+};
+menuBar.addEventListener('click', toggleDropDown);
 const removeModal = () => {
-    dropDown.style.transform = "translateX(100%)"
-     menuBar.childNodes[1].style.color = '#6070ff'
-     menuBar.childNodes[1].className = "fa-solid fa-bars"
-     menuBar.childNodes[1].style.fontSize = '30px'
-     isShown = false;
-}
-portfolioLink.addEventListener("click", removeModal);
-aboutLink.addEventListener("click", removeModal);
-contactLink.addEventListener("click", removeModal);
+  dropDown.style.transform = 'translateX(100%)';
+  menuBar.childNodes[1].style.color = '#6070ff';
+  menuBar.childNodes[1].className = 'fa-solid fa-bars';
+  menuBar.childNodes[1].style.fontSize = '30px';
+  isShown = false;
+};
+
+portfolioLink.addEventListener('click', removeModal);
+aboutLink.addEventListener('click', removeModal);
+contactLink.addEventListener('click', removeModal);
